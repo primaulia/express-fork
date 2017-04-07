4f497fc6e06486cec71bad5e123f6eb062da3238
hexhex23 <mariawilson23@gmail.com>
2017-04-07 11:41:40 +0800

    toUpperCase

--- app.js
+++ app.js
@@ -22,6 +22,20 @@ app.get('/services', function (req, res) {
   res.send('services brian')
 })
 
+app.get('/people/:email', function (req, res) {
+  console.log('req.params are', req.params)
+  console.log('retrieve people with email' + req.params.email)
+  var html = '<html><body><h1>Boo ' + req.params.email + '</h1></body></html>'
+  res.send(html)
+})
+
+app.get('/capitalize/:string', function (req, res) {
+  console.log('req.params are', req.params)
+  console.log(req.params.string)
+  var html = req.params.string.toUpperCase()
+  res.send(html)
+})
+
 app.listen(port, function () {
   console.log('express is running on port ' + port)
 })
