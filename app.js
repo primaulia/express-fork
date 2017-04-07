819a9651c939d1dced5093cae29dcc8c811b61dc
Prima Aulia Gusta <primaulia@gmail.com>
2017-04-07 10:53:13 +0800

    pull this please

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+var port = 4000
+
+app.get('/', function (req, res) {
+  res.send('hello brian')
+})
+
+app.get('/about', function (req, res) {
+  res.send('about brian')
+})
+
+app.get('/faq', function (req, res) {
+  res.send('faq brian')
+})
+
+app.listen(port, function () {
+  console.log('express is running on port ' + port)
+})
