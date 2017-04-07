0fb5de404a4b9213ba183aba65c18c54038e7339
Vikas Garg <vikasgarg@hotmail.com>
2017-04-07 13:36:34 +0800

    revisions

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ var express = require('express')
 var app = express()
 var port = 5000
 
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.sendFile('./homepage.html', { root: __dirname })
+})
+
+app.get('/about', function (req, res) {
+  res.sendFile('./about.html', { root: __dirname })
+})
+
 app.get('/', function (req, res) {
   var html = '<html><body><h1>Hello World</h1></body></html>'
   res.send(html)
