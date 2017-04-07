48aba70e1bb3c9cc57eb4cad261d1e0eec5a0dcf
Darrell Teo <darrelltzj@gmail.com>
2017-04-07 11:38:43 +0800

    nodemon and capitalize

--- app.js
+++ app.js
@@ -3,15 +3,33 @@ var app = express()
 var port = 8080
 
 app.get('/', function (req, res) {
-  res.send('hello brian')
+  var html = '<html><body><h1>Hello</h1></body></html>'
+  res.send(html)
 })
 
 app.get('/about', function (req, res) {
+  console.log(req.params)
   res.send('about brian')
 })
 
 app.get('/faq', function (req, res) {
   res.send('faq brian')
+})
+
+// Wildcard
+app.get('/people/*', function (req, res) {
+  console.log(req.params)
+  console.log(req.params['0'])
+  var html = '<html><body><h1>' + req.params['0'] + '</h1></body></html>'
+  res.send(html)
+})
+
+app.get('/capitalize/:string', function (req, res) {
+  console.log(req.params)
+  console.log(req.params.string)
+  var output = req.params.string.toUpperCase()
+  var html = '<html><body><h1>' + output + '</h1></body></html>'
+  res.send(html)
 })
 
 app.get('/contact', function (req, res) {
