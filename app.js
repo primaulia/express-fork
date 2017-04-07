c9275e7345711a597d39e0fc99f4abf90c18e949
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-07 11:01:44 +0800

    added 2 pages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var port = 4000
+var port = 8080
 
 app.get('/', function (req, res) {
   res.send('hello brian')
@@ -12,6 +12,14 @@ app.get('/about', function (req, res) {
 
 app.get('/faq', function (req, res) {
   res.send('faq brian')
+})
+
+app.get('/contact', function (req, res) {
+  res.send('91234567')
+})
+
+app.get('/services', function (req, res) {
+  res.send('brian services')
 })
 
 app.listen(port, function () {
