162f44d614bcd80fd970e6b7b9ff9f71d48a95fb
Prima Aulia Gusta <primaulia@gmail.com>
2017-04-07 12:57:28 +0800

    latest code

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var app = express()
 var port = 4000
 
 app.get('/', function (req, res) {
-  res.send('hello brian')
+  var html = '<html><body><h1>Hello World</h1></body></html>'
+  res.send(html)
 })
 
 app.get('/about', function (req, res) {
+  console.log(req.params)
   res.send('about brian')
 })
 
 app.get('/faq', function (req, res) {
   res.send('faq brian')
 })
+
+app.get('/people/:email', function (req, res) {
+  console.log('req.params are', req.params)
+  console.log('retrieve people with email ' + req.params.name)
+  var html = '<html><h1>Hello ' + req.params.name + '</h1></html>'
+  res.send(html)
+})
+
+app.get('/capitalize/:string')
 
 app.listen(port, function () {
   console.log('express is running on port ' + port)
