0a64b7086b01cda8d0d7bb699911118d7d031f0e
Cara Chew <carachew@gmail.com>
2017-04-07 11:33:59 +0800

    capitalize params

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express()
 var port = 3000
 
 app.get('/', function (req, res) {
+
   res.send('Hello Cara')
 })
 
@@ -13,6 +14,22 @@ app.get('/about', function (req, res) {
 app.get('/contact', function (req, res) {
   res.send('Contact Cara')
 })
+
+app.get('/people/*', function (req, res) {
+  console.log('req.params are ', req.params)
+  console.log('retrieve people with email ' + req.params.email);
+  var html = '<html><body><h1>Hello ' + req.params[0] + '</h1></body></html>'
+  res.send(html)
+})
+
+app.get('/capitalize/:string', function (req, res) {
+  console.log('req.params are ', req.params)
+  console.log('retrieve people with email ' + req.params.string);
+  var html = '<html><body><h1>Hello ' + req.params.string.toUpperCase() + '</h1></body></html>'
+  res.send(html)
+})
+
+
 
 app.listen(port, function () {
   console.log('express is running on port ' + port)
