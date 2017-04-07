1350d07d0d17bc0fe3531ed2361c13a1d865d842
iancwe <crubbishhute@gmail.com>
2017-04-07 11:36:52 +0800

    added in uppercase

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.get('/contact', function (req, res) {
   res.send('contacting this humble abode')
 })
 
+app.get('/people/:email/:name', function (req, res) {
+  console.log('req.params are', req.params)
+  console.log('retrieve people with email' + req.params.email)
+  var html = '<html><h1>Hello' + req.params.email + '</h1></html>'
+  res.send(html)
+})
+
+app.get('/capitalize/:string', function (req, res) {
+  var str = req.params
+  var cap = str.toUpperCase()
+  var html = '<html><h1>Hello' + cap + '</h1></html>'
+  res.send(html)
+})
+
 app.listen(port, function () {
   console.log('express is running on port ' + port)
 })
