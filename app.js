7733d0afcacb04afaa06483a29b6212b69ba5620
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-07 11:35:17 +0800

    add messy capitalize path

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.get('/faq', function (req, res) {
   res.send('faq jonathan')
 })
 
+app.get('/capitalize/:string', function(req, res) {
+  var str = req.params.string
+  var x = str[0].toUpperCase()
+  var y = str.split('')
+  y.shift()
+  var final = y.reduce(function(a,b) {
+    return a + b
+  }, x)
+  res.send(final)
+})
+
 app.listen(port, function () {
   console.log('express is running on port ' + port)
 })
