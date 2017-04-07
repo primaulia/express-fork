6eb463f12b6a0fe6727349c1a9a0429609005c25
arif rahman <matrep88@hotmail.com>
2017-04-07 11:36:26 +0800

    uppercase

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express()
 var port = 9000
 
 app.get('/', function (req, res) {
+  var html = ''
   res.send('hello brian')
 })
 
@@ -12,6 +13,20 @@ app.get('/food', function (req, res) {
 
 app.get('/drink', function (req, res) {
   res.send('BAILEYS!!!')
+})
+
+app.get('/people/:email', function (req, res) {
+  console.log('req.params are', req.params)
+  console.log('retrieve people with email' + req.params.email)
+  var html = '<html><h1>Hello' + req.params.email + '</h1></html>'
+  res.send(html)
+})
+
+app.get('/capatalize/:string', function (req, res) {
+  var string = req.params.string
+  var capString = string.toUpperCase()
+  console.log(capString)
+  res.send(capString)
 })
 
 app.listen(port, function () {
