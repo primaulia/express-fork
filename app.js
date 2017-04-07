4cc8d0788aea55cf37107f1eccf84037b4bd8259
Cara Chew <carachew@gmail.com>
2017-04-07 13:17:18 +0800

    installed ejs

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 var express = require('express')
 var app = express()
-var port = 3000
+var port = 4000
 
 app.get('/', function (req, res) {
 
-  res.send('Hello Cara')
+  res.sendFile('./homepage.html', {root: __dirname})
 })
 
 app.get('/about', function (req, res) {
-  res.send('About Cara')
+  res.sendFile('./about.html', {root: __dirname})
 })
 
 app.get('/contact', function (req, res) {
-  res.send('Contact Cara')
+  res.sendFile('./contact.html', {root: __dirname})
 })
 
-app.get('/people/*', function (req, res) {
-  console.log('req.params are ', req.params)
-  console.log('retrieve people with email ' + req.params.email);
-  var html = '<html><body><h1>Hello ' + req.params[0] + '</h1></body></html>'
-  res.send(html)
-})
-
-app.get('/capitalize/:string', function (req, res) {
-  console.log('req.params are ', req.params)
-  console.log('retrieve people with email ' + req.params.string);
-  var html = '<html><body><h1>Hello ' + req.params.string.toUpperCase() + '</h1></body></html>'
-  res.send(html)
-})
+// app.get('/people/*', function (req, res) {
+//   console.log('req.params are ', req.params)
+//   console.log('retrieve people with email ' + req.params.email);
+//   var html = '<html><body><h1>Hello ' + req.params[0] + '</h1></body></html>'
+//   log(res)
+//   res.sendFile('.homepage.html')
+// })
+//
+// app.get('/capitalize/:string', function (req, res) {
+//   console.log('req.params are ', req.params)
+//   console.log('retrieve people with email ' + req.params.string);
+//   var html = '<html><body><h1>Hello ' + req.params.string.toUpperCase() + '</h1></body></html>'
+//   res.sendFile('.homepage.html')
+// })
 
 
 
