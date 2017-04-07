159182bee1dc66d985f8cae7905476d0111b0e04
Alvin Tan <alvintan@Alvins-MacBook-Pro.local>
2017-04-07 11:01:32 +0800

    added new port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
-var port = 4000
+var port1 = 4000
+var port2 = 3000
 
 app.get('/', function (req, res) {
   res.send('hello brian')
@@ -13,7 +14,14 @@ app.get('/about', function (req, res) {
 app.get('/faq', function (req, res) {
   res.send('faq brian')
 })
+app.get('/hello', function (req, res) {
+  res.send('hello world')
+})
 
-app.listen(port, function () {
-  console.log('express is running on port ' + port)
+app.listen(port1, function () {
+  console.log('express is running on port ' + port1)
+})
+
+app.listen(port2, function () {
+  console.log('express is running on port ' + port2)
 })
