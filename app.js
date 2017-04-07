db2752326f586b1c8995b24054dbc49a6473b9d4
Tom Lee <d.dorkblue@gmail.com>
2017-04-07 11:00:42 +0800

    added /contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var port = 4000
+var port = 1791
 
 app.get('/', function (req, res) {
   res.send('hello brian')
@@ -12,6 +12,10 @@ app.get('/about', function (req, res) {
 
 app.get('/faq', function (req, res) {
   res.send('faq brian')
+})
+
+app.get('/contact', function (req, res) {
+  res.send('please dont contact me')
 })
 
 app.listen(port, function () {
